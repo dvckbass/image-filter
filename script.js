742db8f870054f8adfc7ca7c98808a68807464ac
Ardilla Latifasari <ardillal@beckhoff.com>
2022-04-23 11:59:45 +0200

    init buttons function

---
+++ script.js
@@ -0,0 +1,23 @@
+function loadImage() {
+    alert("File loaded");
+}
+
+function makeGrayscale() {
+    alert("Grayscale");
+}
+
+function makeRed() {
+    alert("Red");
+}
+
+function makeGreen() {
+    alert("Green");
+}
+
+function makeBlue() {
+    alert("Blue");
+}
+
+function reset() {
+    alert("Reset");
+}
